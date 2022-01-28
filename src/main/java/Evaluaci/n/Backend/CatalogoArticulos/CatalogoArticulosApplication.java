de0e37d9cc5c49aeb2e7673638018f12a332df4b
PabloJz98xD <juan-pablo-z-s58@gmail.com>
2022-01-28 10:56:07 -0600

    Inicio de proyecto XD

---
+++ src/main/java/Evaluaci/n/Backend/CatalogoArticulos/CatalogoArticulosApplication.java
@@ -0,0 +1,13 @@
+package Evaluaci.n.Backend.CatalogoArticulos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoArticulosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoArticulosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Evaluaci/n/Backend/CatalogoArticulos/CatalogoArticulosApplicationTests.java
@@ -0,0 +1,13 @@
+package Evaluaci.n.Backend.CatalogoArticulos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoArticulosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

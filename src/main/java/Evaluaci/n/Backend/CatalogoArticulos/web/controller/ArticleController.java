27fc3e20193bd99e3f62a2e8d8d1943ca1c93ed8
PabloJz98xD <juan-pablo-z-s58@gmail.com>
2022-01-29 13:52:31 -0600

    Desarrollo de controladores respuestas HTTP

--- src/main/java/Evaluaci/n/Backend/CatalogoArticulos/web/controller/ArticleController.java
+++ src/main/java/Evaluaci/n/Backend/CatalogoArticulos/web/controller/ArticleController.java
@@ -3,33 +3,49 @@ package Evaluaci.n.Backend.CatalogoArticulos.web.controller;
 import Evaluaci.n.Backend.CatalogoArticulos.domain.Article;
 import Evaluaci.n.Backend.CatalogoArticulos.domain.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@RestController
+@RequestMapping("/articles")
 public class ArticleController {
     @Autowired
-    private ArticleService articleService;
+    private articleservice articleservice;
 
-    public List<Article> getAll(){
-        return articleService.getAll();
+    @GetMapping("/all")
+    public ResponseEntity<List<Article>> getAll() {
+        return new ResponseEntity<>(articleservice.getAll(), HttpStatus.OK);
     }
 
-    public Optional<Article> getArticle(int articleId){
-        return articleService.getArticle(articleId);
+    @GetMapping("/{id}")
+    public ResponseEntity<Article> getArticle(@PathVariable("id") int ArticleId) {
+        return articleservice.getArticle(ArticleId)
+                .map(Article -> new ResponseEntity<>(Article, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    public Optional<List<Article>> getByUnit(int unitId){
-        return articleService.getByUnit(unitId);
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<List<Article>> getByCategory(@PathVariable("categoryId") int categoryId) {
+        return articleservice.getByCategory(categoryId)
+                .map(articles -> new ResponseEntity<>(articles, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    public Article save(Article article){
-        return articleService.save(article);
+    @PostMapping("/save")
+    public ResponseEntity<Article> save(@RequestBody Article Article) {
+        return new ResponseEntity<>(articleservice.save(Article), HttpStatus.CREATED);
     }
 
-    public boolean delete(int articleId){
-        return articleService.delete(articleId);
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable("id") int ArticleId) {
+        if (articleservice.delete(ArticleId)) {
+            return new ResponseEntity(HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 }
